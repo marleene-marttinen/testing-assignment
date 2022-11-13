7e73ed00c0d344583e6b60f79149fe91aedb854c
Marleene Marttinen <marleenemarttinen@gmail.com>
2022-11-13 17:17:47 +0200

    First test for isObjectLike

---
+++ test/isObjectLike.test.js
@@ -0,0 +1,10 @@
+var chai = require("chai");
+const isObjectLike = require("../isObjectLike.js");
+const expect = chai.expect
+
+describe("iOL", () => {
+
+    it("Should return false when value is null", ()=>{
+        expect(isObjectLike(null)).to.be.false;
+    });
+})
