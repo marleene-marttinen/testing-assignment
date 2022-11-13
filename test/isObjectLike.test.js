e6ad6be79a72679399b490519023dc9da4d164f4
Marleene Marttinen <marleenemarttinen@gmail.com>
2022-11-13 19:52:09 +0200

    Added mochawesome-report-generator to isObjectLike.test.js

--- test/isObjectLike.test.js
+++ test/isObjectLike.test.js
@@ -2,6 +2,7 @@ var chai = require("chai");
 const isObjectLike = require("../isObjectLike.js");
 const expect = chai.expect
 
+
 describe("iOL", () => {
 
     it("Should return false when value is null", ()=>{
